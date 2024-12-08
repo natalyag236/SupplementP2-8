c4c606748bc6b09e52b9cfebb3a0d383a7a26ad6
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-07 20:29:26 -0500

    doc:added documentation to datetime

--- DateTime.java
+++ DateTime.java
@@ -2,6 +2,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTime {
+    /**
+     * Returns the current date and time formatted as "MM-dd-yyyy:mm:ss".
+     *
+     * @return A string representing the current date and time in the format "MM-dd-yyyy:mm:ss".
+     */
     public static String getCurrentDateTime() {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy:mm:ss");
