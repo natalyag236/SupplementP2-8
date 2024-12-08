851c3f71ecfd8f8122a866fa537508906b9ec962
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-07 20:37:07 -0500

    doc: added documentation to ConvertDateISO class

--- ConvertDateISO.java
+++ ConvertDateISO.java
@@ -3,6 +3,12 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class ConvertDateISO {
+    /**
+     * Converts a given LocalDateTime to an ISO 8601 string format in UTC.
+     *
+     * @param dateTime The LocalDateTime to convert.
+     * @return A string representing the date and time in ISO 8601 format with UTC timezone.
+     */
     public static String convertISO(LocalDateTime dateTime){
         return dateTime.atZone(ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE_TIME);
     }
