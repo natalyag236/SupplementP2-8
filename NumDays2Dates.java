5af1ffa322766b29fc92db7955847fb44afe9d13
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-07 20:56:54 -0500

    doc: added documentation to NumDays2Dates class

--- NumDays2Dates.java
+++ NumDays2Dates.java
@@ -2,6 +2,14 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class NumDays2Dates {
+    /**
+     * Calculates the number of days between two LocalDate instances.
+     *
+     * @param startDate The start date.
+     * @param endDate The end date.
+     * @return The number of days between the start date and end date.
+     */
+
     public static long getDaysBetween(LocalDate startDate, LocalDate endDate){
         return ChronoUnit.DAYS.between(startDate, endDate);
     }
